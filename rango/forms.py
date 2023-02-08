47e1d5346202422082c56cc35d4257d460607446
Zebedee Nye <2675732N@student.gla.ac.uk>
2023-02-08 16:01:32 +0000

    chapter 7 done

--- rango/forms.py
+++ rango/forms.py
@@ -20,5 +20,11 @@ class PageForm(forms.ModelForm):
         model = Page
         exclude = ('Category',)
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
 
-        
+        return cleaned_data
